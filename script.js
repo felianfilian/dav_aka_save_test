31c38bcc0a7eb7b7df0f98daba4bfd044092de21
Dennis Schmalstieg <darianbellentor@outlook.de>
2023-07-20 20:17:31 +0200

    get data test

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 function init() {}
+
+function addTask() {
+  let in_name = document.querySelector(".name").value;
+  let in_prof = document.querySelector(".profession").value;
+
+  console.log(in_name + " - " + in_prof);
+}
