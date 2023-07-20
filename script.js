84ce4cb8b29d957b6a0a4316aac46dc88de7e85f
Dennis Schmalstieg <darianbellentor@outlook.de>
2023-07-20 21:05:53 +0200

    save data

--- script.js
+++ script.js
@@ -1,8 +1,22 @@
+heroes = [];
+
 function init() {}
 
 function addTask() {
-  let in_name = document.querySelector(".name").value;
-  let in_prof = document.querySelector(".profession").value;
+  let in_name = document.querySelector("#name").value;
+  let in_prof = document.querySelector("#profession").value;
 
   console.log(in_name + " - " + in_prof);
+
+  let hero = {
+    name: in_name,
+    profession: in_prof,
+  };
+
+  heroes.push(hero);
+
+  console.log(heroes);
+
+  let heroString = JSON.stringify(heroes);
+  localStorage.setItem("heroes", heroString);
 }
